57e91243174dd35664dddca2ec96f82745db4d38
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-24 14:33:08 +0700

    Add files via upload

---
+++ kode.40.dilatasi.py
@@ -0,0 +1,19 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+x = float(input("Tuliskan Nilai X : "))
+y = float(input("Tuliskan Nilai Y: "))
+k = float(input("Tuliskan Nilai Skala (K) : "))
+
+# Mengkonversi
+x1 = x * k
+y1 = y * k 
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Hasil Pencerminan A(%d,%d)' %(x,y))
+print('Maka X* = %d'  %(x1))
+print('Maka Y* = %d'  %(y1))
+print('==========================================================')
